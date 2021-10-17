65b9e4b093f8f97b14919a15d5de0c9fb6fc5f03
David Clement <81521578+dayvid-d1@users.noreply.github.com>
2021-10-17 16:15:13 +0200

    Execute.sh

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -12,9 +12,9 @@ CADDY_USER="app"
 CADDY_SECRET="JDJhJDEwJDF5U3AuelBxMENaN2o4M1lHWS92cE9iU1QyNjRSWTlCRFJYdmFYT0l3VlRtaXBCcXlGMGRx"
 CADDY_PORT=8129
 
-chmod +x ./caddy-image.sh
-dos2unix .caddy/caddy-image.sh
-. ./caddy/caddy-image.sh \
+chmod +x ./scripts/caddy-image.sh
+dos2unix ./scripts/caddy-image.sh
+. ./scripts/caddy-image.sh \
     -i ${CADDY_IMAGE} \
     -u ${CADDY_USER} \
     -s ${CADDY_SECRET} \
@@ -33,9 +33,9 @@ ROBOT_THREADS=4
 RF_PORT=8181
 RESOURCES=""$(dirname "$PWD")/workspace"
 
-chmod +x ./rfcode/rf-image.sh
-dos2unix ./rfcode/rf-image.sh
-. ./rfcode/rf-image.sh \
+chmod +x ./scripts/rf-image.sh
+dos2unix ./scripts/rf-image.sh
+. ./scripts/rf-image.sh \
     -i ${RF_IMAGE} \
     -t ${ROBOT_THREADS} \
     -p ${RF_PORT} \
