715b6c356764ed00a268c463a53fd275d053fafa
David Clement <81521578+dayvid-d1@users.noreply.github.com>
2021-10-17 19:43:48 +0200

    Setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -x
+set -o nounset
+set -o errexit
+
+timestamp() {
+	date +"%Y-%m-%d %T"
+}
+
+echo "Setting up container dependencies"
+apt-get update -y
+xargs apt-get install -y --no-install-recommends </etc/package-list
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+apt-get update -y
+apt-get install -y nodejs
+pip3 install --disable-pip-version-check --no-cache-dir --no-warn-script-location -r /etc/requirements.txt
