8123212216ff66d8fe46147053011ad980cfe0df
David Clement <dayvid.d1@gmail.com>
2021-10-20 20:04:58 +0200

    rfcode_n37

--- install/setup.sh
+++ install/setup.sh
@@ -11,7 +11,7 @@ timestamp() {
 apt-get update
 xargs apt-get install -y --no-install-recommends </etc/package-list
 echo "$(timestamp) Setting up container dependencies"
-echo "deb https://deb.nodesource.com/node_12.x buster main" > /etc/apt/sources.list.d/nodesource.list
+echo "deb https://deb.nodesource.com/node_14.x buster main" > /etc/apt/sources.list.d/nodesource.list
 wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 apt-get install -yqq nodejs npm
 pip3 install --disable-pip-version-check --no-cache-dir --no-warn-script-location -r /etc/requirements.txt
