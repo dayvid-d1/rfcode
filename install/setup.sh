371fc524c299d4ddb91bf2c73b257f487401e6ed
David Clement <dayvid.d1@gmail.com>
2021-10-23 02:26:15 +0200

    rfcode_n65

--- install/setup.sh
+++ install/setup.sh
@@ -12,6 +12,15 @@ timestamp() {
 echo "$(timestamp) Setting up container dependencies"
 apt-get update
 xargs apt-get install -y --no-install-recommends </etc/package-list
+
+wget https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tgz
+tar xzf Python-3.8.9.tgz
+cd Python-3.8.9.tgz
+/configure --enable-optimizations
+make -j 2
+make alt install
+
+
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -yqq nodejs
 
