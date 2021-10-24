7dc029f960029ff110014cd86c28fbc533e48647
David Clement <dayvid.d1@gmail.com>
2021-10-25 00:40:08 +0200

    edit path3

--- install/rf-image.sh
+++ install/rf-image.sh
@@ -213,4 +213,6 @@ docker run \
   -e ROBOT_OPTIONS="--loglevel DEBUG" \
   -e CROSS_BROWSER=${CROSS_BROWSER} \
   -e AUTO_BROWSER=${AUTO_BROWSER} \
-  $RF_IMAGE /etc/run-tests
+  $RF_IMAGE
+
+docker run --name rfcode-app "//etc//run-tests"
